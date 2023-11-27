0cd46d64befce78fdfededa187a9024d6a812933
Alex Garrett-Smith <alex@codecourse.com>
2023-11-27 18:27:40 +0000

    All code

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+Route::resource('admin/users', 'Admin\UserController');
+Route::resource('admin/plans', 'Admin\PlanController');
+
+/**
+ * DataTable controllers
+ */
+Route::resource('datatable/users', 'DataTable\UserController');
+Route::resource('datatable/plans', 'DataTable\PlanController');
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
